3781cf82ad4b233e017d4c8a8dd21a1bc4590d36
Lucas LCC <lucas.lcc@hotmail.comO>
2019-11-08 01:57:27 -0300

    Reorganizado os arquivos, adicionado 7 inserts

--- sql/DDL - creates.sql
+++ sql/DDL - creates.sql
@@ -2,7 +2,7 @@
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
 -- Schema eventos
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Local` (
   `idLocal` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(155) NOT NULL,
   PRIMARY KEY (`idLocal`),
-  UNIQUE INDEX `idLocal_UNIQUE` (`idLocal` ASC) )
+  UNIQUE INDEX `idLocal_UNIQUE` (`idLocal` ASC))
 ENGINE = InnoDB;
 
 
@@ -35,8 +35,8 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Evento` (
   `dataFim` DATETIME NULL,
   `Local_idLocal` INT NOT NULL,
   PRIMARY KEY (`idEvento`),
-  UNIQUE INDEX `idEvento_UNIQUE` (`idEvento` ASC) ,
-  INDEX `fk_Evento_Local1_idx` (`Local_idLocal` ASC) ,
+  UNIQUE INDEX `idEvento_UNIQUE` (`idEvento` ASC),
+  INDEX `fk_Evento_Local1_idx` (`Local_idLocal` ASC),
   CONSTRAINT `fk_Evento_Local1`
     FOREIGN KEY (`Local_idLocal`)
     REFERENCES `eventos`.`Local` (`idLocal`)
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Pessoa` (
   `cpf` VARCHAR(14) NOT NULL,
   `telefone` VARCHAR(45) NULL,
   PRIMARY KEY (`idPessoa`),
-  UNIQUE INDEX `idPessoa_UNIQUE` (`idPessoa` ASC) )
+  UNIQUE INDEX `idPessoa_UNIQUE` (`idPessoa` ASC))
 ENGINE = InnoDB;
 
 
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Instituicao` (
   `idInstituicao` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`idInstituicao`),
-  UNIQUE INDEX `idInstituicao_UNIQUE` (`idInstituicao` ASC) )
+  UNIQUE INDEX `idInstituicao_UNIQUE` (`idInstituicao` ASC))
 ENGINE = InnoDB;
 
 
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `eventos`.`GrauInstrucao` (
   `idGrauInstrucao` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(155) NOT NULL,
   PRIMARY KEY (`idGrauInstrucao`),
-  UNIQUE INDEX `idGrauInstrucao_UNIQUE` (`idGrauInstrucao` ASC) )
+  UNIQUE INDEX `idGrauInstrucao_UNIQUE` (`idGrauInstrucao` ASC))
 ENGINE = InnoDB;
 
 
@@ -89,9 +89,9 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Participante` (
   `Instituicao_idInstituicao` INT NOT NULL,
   `GrauInstrucao_idGrauInstrucao` INT NOT NULL,
   PRIMARY KEY (`Pessoa_idPessoa`),
-  UNIQUE INDEX `Pessoa_idPessoa_UNIQUE` (`Pessoa_idPessoa` ASC) ,
-  INDEX `fk_Participante_Instituicao1_idx` (`Instituicao_idInstituicao` ASC) ,
-  INDEX `fk_Participante_GrauInstrucao1_idx` (`GrauInstrucao_idGrauInstrucao` ASC) ,
+  UNIQUE INDEX `Pessoa_idPessoa_UNIQUE` (`Pessoa_idPessoa` ASC),
+  INDEX `fk_Participante_Instituicao1_idx` (`Instituicao_idInstituicao` ASC),
+  INDEX `fk_Participante_GrauInstrucao1_idx` (`GrauInstrucao_idGrauInstrucao` ASC),
   CONSTRAINT `fk_Participante_Pessoa`
     FOREIGN KEY (`Pessoa_idPessoa`)
     REFERENCES `eventos`.`Pessoa` (`idPessoa`)
@@ -117,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Funcao` (
   `idFuncao` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(85) NULL,
   PRIMARY KEY (`idFuncao`),
-  UNIQUE INDEX `idFuncao_UNIQUE` (`idFuncao` ASC) )
+  UNIQUE INDEX `idFuncao_UNIQUE` (`idFuncao` ASC))
 ENGINE = InnoDB;
 
 
@@ -129,8 +129,8 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Organizador` (
   `Evento_idEvento` INT NOT NULL,
   `Funcao_idFuncao` INT NOT NULL,
   PRIMARY KEY (`Pessoa_idPessoa`, `Evento_idEvento`, `Funcao_idFuncao`),
-  INDEX `fk_Organizador_Evento1_idx` (`Evento_idEvento` ASC) ,
-  INDEX `fk_Organizador_Funcao1_idx` (`Funcao_idFuncao` ASC) ,
+  INDEX `fk_Organizador_Evento1_idx` (`Evento_idEvento` ASC),
+  INDEX `fk_Organizador_Funcao1_idx` (`Funcao_idFuncao` ASC),
   CONSTRAINT `fk_Organizador_Pessoa1`
     FOREIGN KEY (`Pessoa_idPessoa`)
     REFERENCES `eventos`.`Pessoa` (`idPessoa`)
@@ -171,7 +171,7 @@ CREATE TABLE IF NOT EXISTS `eventos`.`TipoAtividade` (
   `idTipoAtividade` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(145) NOT NULL,
   PRIMARY KEY (`idTipoAtividade`),
-  UNIQUE INDEX `idTipoEvento_UNIQUE` (`idTipoAtividade` ASC) )
+  UNIQUE INDEX `idTipoEvento_UNIQUE` (`idTipoAtividade` ASC))
 ENGINE = InnoDB;
 
 
@@ -183,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Sala` (
   `numero` INT NOT NULL,
   `nome` VARCHAR(65) NOT NULL,
   PRIMARY KEY (`idSala`),
-  UNIQUE INDEX `idSala_UNIQUE` (`idSala` ASC) )
+  UNIQUE INDEX `idSala_UNIQUE` (`idSala` ASC))
 ENGINE = InnoDB;
 
 
@@ -194,7 +194,7 @@ CREATE TABLE IF NOT EXISTS `eventos`.`CargaHoraria` (
   `idCargaHoraria` INT NOT NULL AUTO_INCREMENT,
   `duracao` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idCargaHoraria`),
-  UNIQUE INDEX `idCargaHoraria_UNIQUE` (`idCargaHoraria` ASC) )
+  UNIQUE INDEX `idCargaHoraria_UNIQUE` (`idCargaHoraria` ASC))
 ENGINE = InnoDB;
 
 
@@ -204,20 +204,22 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `eventos`.`Atividade` (
   `idAtividade` INT NOT NULL AUTO_INCREMENT,
   `titulo` VARCHAR(255) NOT NULL,
+  `valor` DOUBLE NULL,
   `descricao` TEXT NULL,
   `limiteInscricoes` INT NOT NULL,
+  `data` DATE NOT NULL,
   `TipoAtividade_idTipoAtividade` INT NOT NULL,
   `Evento_idEvento` INT NOT NULL,
   `Palestrante_Pessoa_idPessoa` INT NOT NULL,
   `Sala_idSala` INT NOT NULL,
   `CargaHoraria_idCargaHoraria` INT NOT NULL,
   PRIMARY KEY (`idAtividade`),
-  UNIQUE INDEX `idAtividade_UNIQUE` (`idAtividade` ASC) ,
-  INDEX `fk_Atividade_TipoEvento1_idx` (`TipoAtividade_idTipoAtividade` ASC) ,
-  INDEX `fk_Atividade_Evento1_idx` (`Evento_idEvento` ASC) ,
-  INDEX `fk_Atividade_Palestrante1_idx` (`Palestrante_Pessoa_idPessoa` ASC) ,
-  INDEX `fk_Atividade_Sala1_idx` (`Sala_idSala` ASC) ,
-  INDEX `fk_Atividade_CargaHoraria1_idx` (`CargaHoraria_idCargaHoraria` ASC) ,
+  UNIQUE INDEX `idAtividade_UNIQUE` (`idAtividade` ASC),
+  INDEX `fk_Atividade_TipoEvento1_idx` (`TipoAtividade_idTipoAtividade` ASC),
+  INDEX `fk_Atividade_Evento1_idx` (`Evento_idEvento` ASC),
+  INDEX `fk_Atividade_Palestrante1_idx` (`Palestrante_Pessoa_idPessoa` ASC),
+  INDEX `fk_Atividade_Sala1_idx` (`Sala_idSala` ASC),
+  INDEX `fk_Atividade_CargaHoraria1_idx` (`CargaHoraria_idCargaHoraria` ASC),
   CONSTRAINT `fk_Atividade_TipoEvento1`
     FOREIGN KEY (`TipoAtividade_idTipoAtividade`)
     REFERENCES `eventos`.`TipoAtividade` (`idTipoAtividade`)
@@ -255,9 +257,9 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Inscricao` (
   `Participante_Pessoa_idPessoa` INT NOT NULL,
   `Evento_idEvento` INT NOT NULL,
   PRIMARY KEY (`idInscricao`),
-  UNIQUE INDEX `idInscricao_UNIQUE` (`idInscricao` ASC) ,
-  INDEX `fk_Inscricao_Participante1_idx` (`Participante_Pessoa_idPessoa` ASC) ,
-  INDEX `fk_Inscricao_Evento1_idx` (`Evento_idEvento` ASC) ,
+  UNIQUE INDEX `idInscricao_UNIQUE` (`idInscricao` ASC),
+  INDEX `fk_Inscricao_Participante1_idx` (`Participante_Pessoa_idPessoa` ASC),
+  INDEX `fk_Inscricao_Evento1_idx` (`Evento_idEvento` ASC),
   CONSTRAINT `fk_Inscricao_Participante1`
     FOREIGN KEY (`Participante_Pessoa_idPessoa`)
     REFERENCES `eventos`.`Participante` (`Pessoa_idPessoa`)
@@ -282,9 +284,9 @@ CREATE TABLE IF NOT EXISTS `eventos`.`Despesa` (
   `Atividade_idAtividade` INT NULL,
   `Evento_idEvento` INT NOT NULL,
   PRIMARY KEY (`idDespesa`),
-  UNIQUE INDEX `idDespesa_UNIQUE` (`idDespesa` ASC) ,
-  INDEX `fk_Despesa_Atividade1_idx` (`Atividade_idAtividade` ASC) ,
-  INDEX `fk_Despesa_Evento1_idx` (`Evento_idEvento` ASC) ,
+  UNIQUE INDEX `idDespesa_UNIQUE` (`idDespesa` ASC),
+  INDEX `fk_Despesa_Atividade1_idx` (`Atividade_idAtividade` ASC),
+  INDEX `fk_Despesa_Evento1_idx` (`Evento_idEvento` ASC),
   CONSTRAINT `fk_Despesa_Atividade1`
     FOREIGN KEY (`Atividade_idAtividade`)
     REFERENCES `eventos`.`Atividade` (`idAtividade`)
@@ -306,11 +308,10 @@ CREATE TABLE IF NOT EXISTS `eventos`.`InscricaoAtividade` (
   `Atividade_idAtividade` INT NOT NULL,
   `Inscricao_idInscricao` INT NOT NULL,
   `dataPagamento` DATE NULL,
-  `valor` DOUBLE NULL,
   PRIMARY KEY (`idInscricaoAtividade`),
-  UNIQUE INDEX `idInscricaoAtividade_UNIQUE` (`idInscricaoAtividade` ASC) ,
-  INDEX `fk_InscricaoAtividade_Atividade1_idx` (`Atividade_idAtividade` ASC) ,
-  INDEX `fk_InscricaoAtividade_Inscricao1_idx` (`Inscricao_idInscricao` ASC) ,
+  UNIQUE INDEX `idInscricaoAtividade_UNIQUE` (`idInscricaoAtividade` ASC),
+  INDEX `fk_InscricaoAtividade_Atividade1_idx` (`Atividade_idAtividade` ASC),
+  INDEX `fk_InscricaoAtividade_Inscricao1_idx` (`Inscricao_idInscricao` ASC),
   CONSTRAINT `fk_InscricaoAtividade_Atividade1`
     FOREIGN KEY (`Atividade_idAtividade`)
     REFERENCES `eventos`.`Atividade` (`idAtividade`)
---
+++ sql/DQL - selects.sql
@@ -0,0 +1,154 @@
+
+#   `TABELA Y` INNER JOIN `TABELA X` ON `ID TABELA X` = `FK TABELA Y`
+
+#   2.1) Lista com dados de todos os participantes cadastrados no respectivo evento;
+
+SET @evento = 1;
+
+#   TOTAL 15 para cada evento
+
+SET @evento = 1;
+
+SELECT pessoa.*, participante.email, evento.nome 
+FROM pessoa 
+INNER JOIN participante ON participante.Pessoa_idPessoa = pessoa.idPessoa 
+INNER JOIN inscricao ON inscricao.Participante_Pessoa_idPessoa = participante.Pessoa_idPessoa
+INNER JOIN evento ON evento.idEvento = inscricao.Evento_idEvento
+WHERE idEvento=@evento
+
+
+#   2.2) Lista com o nome, e-mail e telefone dos participantes com pagamento de inscrição pendente para o evento;
+
+#   TOTAL 8 sem pagamento
+
+SET @evento = 1;
+
+SELECT pessoa.nome, pessoa.telefone, participante.email, atividade.titulo, inscricaoatividade.dataPagamento
+FROM pessoa
+INNER JOIN participante ON participante.Pessoa_idPessoa = pessoa.idPessoa
+INNER JOIN inscricao ON inscricao.Participante_Pessoa_idPessoa = participante.Pessoa_idPessoa
+INNER JOIN inscricaoatividade ON inscricaoatividade.Inscricao_idInscricao = inscricao.idInscricao
+INNER JOIN atividade ON atividade.idAtividade = inscricaoatividade.Atividade_idAtividade
+INNER JOIN evento ON evento.idEvento = atividade.Evento_idEvento
+WHERE inscricaoatividade.dataPagamento IS NULL
+AND idEvento=@evento
+
+
+#   2.3) Obter uma lista com o número de participantes ativos (inscrição paga) agrupada por atividade do evento (listar todas as atividades mesmo as que não possuem inscrições);
+
+SET @evento = 1;
+
+SELECT evento.nome, atividade.titulo, atividade.limiteInscricoes, 
+    ( SELECT COUNT( inscricaoatividade.Atividade_idAtividade) 
+        FROM inscricaoatividade 
+        WHERE inscricaoatividade.Atividade_idAtividade=atividade.idAtividade ) AS quantidadeInscritos
+FROM atividade
+LEFT JOIN inscricaoatividade ON inscricaoatividade.Atividade_idAtividade=atividade.idAtividade
+INNER JOIN evento ON evento.idEvento = atividade.Evento_idEvento
+WHERE idEvento = @evento
+GROUP BY atividade.titulo
+
+#   2.4) Consultar qual atividade do evento tem o maior número de inscritos;
+
+
+
+#   2.4) Consultar qual atividade do evento tem o maior número de inscritos;
+
+SET @evento = 1;
+
+SELECT evento.nome, atividade.titulo, atividade.limiteInscricoes, 
+    ( SELECT COUNT( inscricaoatividade.Atividade_idAtividade) 
+        FROM inscricaoatividade 
+        WHERE inscricaoatividade.Atividade_idAtividade=atividade.idAtividade ) AS quantidadeInscritos
+FROM atividade
+LEFT JOIN inscricaoatividade ON inscricaoatividade.Atividade_idAtividade=atividade.idAtividade
+INNER JOIN evento ON evento.idEvento = atividade.Evento_idEvento
+WHERE evento.idEvento = @evento
+GROUP BY atividade.titulo
+ORDER BY quantidadeInscritos DESC
+LIMIT 1
+
+
+#   2.5) Lista com o roteiro/sequencia das atividades selecionadas na inscrição por um participante em um evento;
+
+SET @evento = 1;
+
+SELECT evento.nome, tipoatividade.nome, atividade.titulo, atividade.data, CONCAT( sala.nome," - ", sala.numero ) AS sala, pessoa.nome, pessoa.cpf, pessoa.telefone, participante.email
+FROM atividade
+INNER JOIN sala ON sala.idSala = atividade.Sala_idSala
+INNER JOIN tipoatividade ON tipoatividade.idTipoAtividade = atividade.TipoAtividade_idTipoAtividade
+INNER JOIN evento ON evento.idEvento = atividade.Evento_idEvento
+INNER JOIN inscricaoatividade ON inscricaoatividade.Atividade_idAtividade = atividade.idAtividade
+INNER JOIN inscricao ON inscricao.idInscricao = inscricaoatividade.Inscricao_idInscricao
+INNER JOIN participante ON participante.Pessoa_idPessoa = inscricao.Participante_Pessoa_idPessoa
+INNER JOIN pessoa ON pessoa.idPessoa = participante.Pessoa_idPessoa
+WHERE pessoa.idPessoa = 1 AND evento.idEvento = @evento
+ORDER BY atividade.data
+
+
+#	2.6) Lista completa com a programação geral (em ordem cronológica) do evento, título e descrição das atividades, nome do palestrante, data e hora, local / sala;
+
+SET @evento = 1;
+
+SELECT evento.nome AS Evento, tipoatividade.nome AS `Tipo da atividade`, atividade.titulo AS `Atividade`,
+		atividade.data, CONCAT( sala.nome," - ", sala.numero ) AS sala, local.nome AS `Local`, pessoa.nome AS palestrante, atividade.descricao 
+FROM atividade
+INNER JOIN sala ON sala.idSala = atividade.Sala_idSala
+INNER JOIN tipoatividade ON tipoatividade.idTipoAtividade = atividade.TipoAtividade_idTipoAtividade
+INNER JOIN evento ON evento.idEvento = atividade.Evento_idEvento
+INNER JOIN `local` ON local.idLocal = evento.Local_idLocal
+INNER JOIN palestrante ON palestrante.Pessoa_idPessoa = atividade.Palestrante_Pessoa_idPessoa
+INNER JOIN pessoa ON pessoa.idPessoa = palestrante.Pessoa_idPessoa
+WHERE evento.idEvento = @evento
+ORDER BY atividade.data
+
+
+#	2.7) Lista com informações das atividades que acontecerão em um determinado dia do evento;
+
+SET @evento = 1;
+
+SELECT evento.nome AS Evento, tipoatividade.nome AS `Tipo da atividade`, atividade.titulo AS `Atividade`,
+		atividade.data, CONCAT( sala.nome," - ", sala.numero ) AS sala, local.nome AS `Local`, pessoa.nome AS palestrante, atividade.descricao 
+FROM atividade
+INNER JOIN sala ON sala.idSala = atividade.Sala_idSala
+INNER JOIN tipoatividade ON tipoatividade.idTipoAtividade = atividade.TipoAtividade_idTipoAtividade
+INNER JOIN evento ON evento.idEvento = atividade.Evento_idEvento
+INNER JOIN `local` ON local.idLocal = evento.Local_idLocal
+INNER JOIN palestrante ON palestrante.Pessoa_idPessoa = atividade.Palestrante_Pessoa_idPessoa
+INNER JOIN pessoa ON pessoa.idPessoa = palestrante.Pessoa_idPessoa
+WHERE evento.idEvento = @evento
+AND DATE_FORMAT( atividade.data, '%Y %m %d' ) = DATE_FORMAT( '2018-09-03', '%Y %m %d' )
+#AND atividade.data BETWEEN '2018-09-03' AND '2018-09-09'
+
+
+#	2.8) Lista com os dados completos de uma atividade (onde e quando ocorrerá, nome do palestrante, título e descrição da atividade);
+
+SET @PESQUISA = '';
+
+SELECT evento.nome AS Evento, tipoatividade.nome AS `Tipo da atividade`, atividade.titulo AS `Atividade`,
+		atividade.data, CONCAT( sala.nome," - ", sala.numero ) AS sala, local.nome AS `Local`, pessoa.nome AS palestrante, atividade.descricao 
+FROM atividade
+INNER JOIN sala ON sala.idSala = atividade.Sala_idSala
+INNER JOIN tipoatividade ON tipoatividade.idTipoAtividade = atividade.TipoAtividade_idTipoAtividade
+INNER JOIN evento ON evento.idEvento = atividade.Evento_idEvento
+INNER JOIN `local` ON local.idLocal = evento.Local_idLocal
+INNER JOIN palestrante ON palestrante.Pessoa_idPessoa = atividade.Palestrante_Pessoa_idPessoa
+INNER JOIN pessoa ON pessoa.idPessoa = palestrante.Pessoa_idPessoa
+WHERE evento.idEvento = 1 
+AND (atividade.idAtividade = @PESQUISA OR atividade.titulo LIKE CONCAT('%', @PESQUISA, '%') 
+		OR tipoatividade.nome LIKE CONCAT('%', @PESQUISA, '%')
+        OR pessoa.nome LIKE CONCAT('%', @PESQUISA, '%')
+        OR atividade.idAtividade = @PESQUISA )
+ORDER BY atividade.data
+
+
+#	2.10) Lista com nome, função e contato de todos os organizadores vinculados a um evento; Observações:
+
+SET @evento = 1;
+
+SELECT pessoa.nome, funcao.nome, pessoa.telefone
+FROM organizador
+INNER JOIN pessoa ON pessoa.idPessoa = organizador.Pessoa_idPessoa
+INNER JOIN funcao ON funcao.idfuncao = organizador.Funcao_idFuncao
+INNER JOIN evento ON evento.idEvento = organizador.Evento_idEvento
+WHERE evento.idEvento = @evento
